29dfe1e2a8b7502c9f68bf82c3baa4e88d6e7ae0
Arthur <arthur.deierlein@adfinis.com>
2023-01-13 08:23:22 +0100

    feat: frontend first concept (layout etc.)

--- frontend/app/router.js
+++ frontend/app/router.js
@@ -9,4 +9,5 @@ export default class Router extends EmberRouter {
 Router.map(function () {
   this.route('overview', { path: '/' });
   this.route('create');
+  this.route('details', { path: '/projects/nanosoft' });
 });
