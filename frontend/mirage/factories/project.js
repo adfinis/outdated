8b078bf1cc80f63aa025fb36d5fd9970acbce73b
Arthur <arthur.deierlein@adfinis.com>
2023-01-19 16:45:54 +0100

    feat: responsive layout

--- frontend/mirage/factories/project.js
+++ frontend/mirage/factories/project.js
@@ -2,27 +2,11 @@ import { Factory, trait } from 'miragejs';
 import { faker } from '@faker-js/faker';
 
 export default Factory.extend({
-  name: () =>
-    faker.helpers.arrayElement([
-      faker.random.word() +
-        faker.helpers.arrayElement(['y ', 'x ', 'io ']) +
-        faker.helpers.arrayElement([
-          'webapp',
-          'application',
-          'frontend',
-          'backend',
-        ]),
-      'ember ' +
-        faker.helpers.arrayElement([
-          'uikit',
-          faker.word.adjective() + ' helpers',
-          'caluma',
-        ]),
-      faker.name.jobArea() + 'y',
-      faker.helpers.arrayElement(['ember', 'caluma', 'python', 'django']) +
-        ' ' +
-        faker.word.verb(),
-    ]),
+  name: () => faker.hacker.adjective() + ' ' + faker.company.bsNoun(),
+
+  status: () =>
+    faker.helpers.arrayElement(['OUTDATED', 'WARNING', 'UP-TO-DATE']),
+
   repo() {
     return (
       'https://github.com/' +
@@ -31,9 +15,6 @@ export default Factory.extend({
       faker.helpers.slugify(this.name)
     );
   },
-  status: () =>
-    faker.helpers.arrayElement(['OUTDATED', 'WARNING', 'UP-TO-DATE']),
-
   withVersions: trait({
     afterCreate(project, server) {
       server.createList('version', 3, 'isEndOfLife', { projects: [project] });
