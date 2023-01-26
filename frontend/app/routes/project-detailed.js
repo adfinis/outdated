aa3425076c63132af46de625127d4a610ea81654
Arthur <arthur.deierlein@adfinis.com>
2023-01-26 08:52:34 +0100

    fix: fixed bug (typo) relationship now properly

--- frontend/app/routes/project-detailed.js
+++ frontend/app/routes/project-detailed.js
@@ -5,7 +5,7 @@ export default class ProjectDetailedRoute extends Route {
   @service store;
   model(params) {
     return this.store.findRecord('project', params.id, {
-      include: 'dependecyVersions,dependecyVersions.dependency',
+      include: 'dependencyVersions,dependencyVersions.dependency',
     });
   }
 }
