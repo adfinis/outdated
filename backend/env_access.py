52d2cb7a3227082b447f10668626ce8fe27b9a9a
Arthur <arthur.deierlein@adfinis.com>
2023-02-02 16:29:21 +0100

    refactor: refactored tests, compose setup

--- backend/env_access.py
+++
@@ -1,5 +0,0 @@
-"""Generalized access to enviromental variables."""
-import environ
-
-env = environ.Env()
-env.read_env("../.env")
--- backend/outdated/outdated/apps.py
+++ backend/outdated/outdated/apps.py
@@ -2,5 +2,4 @@ from django.apps import AppConfig
 
 
 class OutdatedConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
     name = "outdated"
