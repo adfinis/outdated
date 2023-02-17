ccd9373eed1dda5157ff9b3b9ceda7d44c6c18dd
Arthur <arthur.deierlein@adfinis.com>
2023-02-17 11:16:53 +0100

    feat: added gunicorn

--- api/outdated/settings.py
+++ api/outdated/settings.py
@@ -1,5 +1,4 @@
 import os
-from pathlib import Path
 
 import environ
 
@@ -10,32 +9,46 @@ ENV_FILE = django_root(".env")
 if os.path.exists(ENV_FILE):
     environ.Env.read_env(ENV_FILE)  # pragma: no cover
 
-DEBUG = True
+ENV = env.str("ENV", "prod")
 
-SECRET_KEY = env(
-    "DJANGO_SECRET_KEY",
-    default="keykeykeykeykeykey",
-)
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+def default(default_dev=env.NOTSET, default_prod=env.NOTSET):
+    """Environment aware default."""
+    return default_prod if ENV == "prod" else default_dev
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["*"])
 
+# Database definiton
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("DATABASE_NAME", default="outdated"),
+        "USER": env("DATABASE_USER", default="outdated"),
+        "PASSWORD": env("DATABASE_PASSWORD", default=default("outdated")),
+        "HOST": env("DATABASE_HOST", default="db"),
+        "PORT": env.str("DATABASE_PORT", default=""),
+    }
+}
 
 # Application definition
 
-
+DEBUG = env.bool("DEBUG", default=default(True, False))
+SECRET_KEY = env.str("SECRET_KEY", default=default("keykeykeykeykeykey"))
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=default(["*"]))
 INSTALLED_APPS = [
     "outdated",
-    "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework_json_api",
 ]
+
+if ENV == "dev":
+    INSTALLED_APPS.append("django.contrib.staticfiles")
+    STATIC_URL = "/static/"
+    STATIC_ROOT = "/app/static"
 
 
 MIDDLEWARE = [
@@ -50,39 +63,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "outdated.urls"
 
-TEMPLATES = [
-    {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": ["outdated/templates/"],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.debug",
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
-            ],
-        },
-    },
-]
-
 WSGI_APPLICATION = "outdated.wsgi.application"
-
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": env("DATABASE_NAME", default="outdated"),
-        "USER": env("DATABASE_USER", default="outdated"),
-        "PASSWORD": env("DATABASE_PASSWORD", default="outdated"),
-        "HOST": env("DATABASE_HOST", default="db"),
-        "PORT": env.str("DATABASE_PORT", default=""),
-    }
-}
-
-
-# Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -105,16 +86,20 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = env.str("DJANGO_TIME_ZONE", "Europe/Zurich")
 
 USE_I18N = True
-
 USE_L10N = True
 
 USE_TZ = True
 
+DECIMAL_SEPARATOR = env.str("DECIMAL_SEPARATOR", ".")
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+
 REST_FRAMEWORK = {
-    "PAGE_SIZE": 30,
     "EXCEPTION_HANDLER": "rest_framework_json_api.exceptions.exception_handler",
     "DEFAULT_PAGINATION_CLASS": "rest_framework_json_api.pagination.JsonApiPageNumberPagination",
     "DEFAULT_PARSER_CLASSES": (
@@ -125,31 +110,45 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_RENDERER_CLASSES": (
         "rest_framework_json_api.renderers.JSONRenderer",
-        "rest_framework_json_api.renderers.BrowsableAPIRenderer",
+        "rest_framework.renderers.JSONRenderer",
     ),
     "DEFAULT_METADATA_CLASS": "rest_framework_json_api.metadata.JSONAPIMetadata",
-    "DEFAULT_SCHEMA_CLASS": "rest_framework_json_api.schemas.openapi.AutoSchema",
     "DEFAULT_FILTER_BACKENDS": (
         "rest_framework_json_api.filters.QueryParameterValidationFilter",
         "rest_framework_json_api.filters.OrderingFilter",
         "rest_framework_json_api.django_filters.DjangoFilterBackend",
         "rest_framework.filters.SearchFilter",
     ),
+    "ORDERING_PARAM": "sort",
     "SEARCH_PARAM": "filter[search]",
     "TEST_REQUEST_RENDERER_CLASSES": (
         "rest_framework_json_api.renderers.JSONRenderer",
         "rest_framework.renderers.JSONRenderer",
-        "rest_framework.renderers.MultiPartRenderer",
     ),
     "TEST_REQUEST_DEFAULT_FORMAT": "vnd.api+json",
 }
 
+if ENV == "dev":
+    REST_FRAMEWORK["DEFAULT_RENDERER_CLASSES"] += (
+        "rest_framework_json_api.renderers.BrowsableAPIRenderer",
+    )
+    TEMPLATES = [
+        {
+            "BACKEND": "django.template.backends.django.DjangoTemplates",
+            "DIRS": [django_root("outdated", "templates")],
+            "APP_DIRS": True,
+            "OPTIONS": {
+                "context_processors": [
+                    "django.template.context_processors.debug",
+                    "django.template.context_processors.request",
+                    "django.contrib.auth.context_processors.auth",
+                    "django.contrib.messages.context_processors.messages",
+                ]
+            },
+        },
+    ]
+
+
 JSON_API_FORMAT_FIELD_NAMES = "dasherize"
 JSON_API_FORMAT_TYPES = "dasherize"
 JSON_API_PLURALIZE_TYPES = True
-
-
-STATIC_URL = "/static/"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
