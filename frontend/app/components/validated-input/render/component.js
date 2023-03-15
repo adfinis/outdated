d0c7e832b1ea07402af89b984efe1260901e288d
Arthur <arthur.deierlein@adfinis.com>
2023-03-15 09:17:10 +0100

    chore(lint): linted frontend

--- frontend/app/components/validated-input/render/component.js
+++ frontend/app/components/validated-input/render/component.js
@@ -2,6 +2,6 @@ import Component from '@glimmer/component';
 
 export default class RenderComponent extends Component {
   get selectComponent() {
-    return !!this.args.multiple ? 'power-select-multiple' : 'power-select';
+    return this.args.multiple ? 'power-select-multiple' : 'power-select';
   }
 }
