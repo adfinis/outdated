c872a2d8d18180fb9df40ba6ebf06cd4ef3246cc
Arthur <arthur.deierlein@adfinis.com>
2023-04-03 13:59:18 +0200

    feat: unsaved project now get deleted on route change

--- frontend/app/routes/projects/create-new.js
+++ frontend/app/routes/projects/create-new.js
@@ -1,3 +1,4 @@
+import { action } from '@ember/object';
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 
@@ -6,5 +7,12 @@ export default class ProjectsCreateNewRoute extends Route {
   model() {
     this.store.findAll('dependency');
     this.store.findAll('dependencyVersion');
+  }
+  @action
+  async deactivate() {
+    await this.store.unloadRecord(
+      // eslint-disable-next-line ember/no-controller-access-in-routes
+      this.store.findRecord('project', this.controller.project.id)
+    );
   }
 }
