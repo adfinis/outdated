61381c121e87fac9853aacefeafa58b092b37226
Arthur <arthur.deierlein@adfinis.com>
2023-04-12 12:38:49 +0200

    feat: sync button for projects

---
+++ frontend/app/components/project-detailed/component.js
@@ -0,0 +1,29 @@
+import { action } from '@ember/object';
+import { service } from '@ember/service';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import fetch from 'fetch';
+export default class ProjectDetailedComponent extends Component {
+  @service notification;
+  @service store;
+  @tracked loading = false;
+
+  @action
+  async syncProject() {
+    try {
+      this.loading = true;
+      const request = await fetch(`/projects/sync/${this.args.project.id}`);
+      if (request.ok) {
+        this.notification.success('Project synced successfully');
+      } else if (request.status === 404) {
+        this.notification.danger('Project not found');
+      } else if (request.status === 500) {
+        this.notification.danger('An error occurred while syncing the project');
+      }
+    } catch (e) {
+      this.notification.danger(e);
+    }
+    this.loading = false;
+    this.store.findRecord('project', this.args.project.id);
+  }
+}
