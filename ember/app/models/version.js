4308969df586198f27b8208292dce2f89833fa69
Arthur <arthur.deierlein@adfinis.com>
2023-04-27 13:50:25 +0200

    fix: frontend is now compatible with the models

--- ember/app/models/version.js
+++ ember/app/models/version.js
@@ -4,4 +4,24 @@ export default class VersionModel extends Model {
   @attr patchVersion;
   @attr('django-date') releaseDate;
   @belongsTo('releaseVersion') releaseVersion;
+
+  get name() {
+    return this.releaseVersion.get('dependency').get('name');
+  }
+  get endOfLife() {
+    return this.releaseVersion.get('endOfLife');
+  }
+
+  get version() {
+    const releaseVersion = this.releaseVersion;
+    return `${releaseVersion.get('majorVersion')}.${releaseVersion.get(
+      'minorVersion'
+    )}.${this.patchVersion}`;
+  }
+  get requirements() {
+    return `${this.name} ${this.version}`;
+  }
+  get status() {
+    return this.releaseVersion.get('status');
+  }
 }
