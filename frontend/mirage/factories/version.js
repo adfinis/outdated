616bf52e9e979ca66137ccba43e7d28dfce6530b
Arthur <arthur.deierlein@adfinis.com>
2023-05-04 11:59:10 +0200

    chore: adjusted mirage for new models

---
+++ frontend/mirage/factories/version.js
@@ -0,0 +1,28 @@
+import { faker } from '@faker-js/faker';
+import { Factory, trait } from 'miragejs';
+
+export default Factory.extend({
+  patchVersion: () => faker.datatype.number({ min: 1, max: 10 }),
+  releaseDate: () => faker.date.past(2),
+
+  afterCreate(version, server) {
+    if (!version.releaseVersion) {
+      version.update({ releaseVersion: server.create('release-version') });
+    }
+  },
+
+  isEndOfLife: trait({
+    afterCreate(version, server) {
+      version.update({
+        releaseVersion: server.create('release-version', 'isEndOfLife'),
+      });
+    },
+  }),
+  isNearlyEndOfLife: trait({
+    afterCreate(version, server) {
+      version.update({
+        releaseVersion: server.create('release-version', 'isNearlyEndOfLife'),
+      });
+    },
+  }),
+});
