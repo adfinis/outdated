d75dfc181b60bc73f2c7a564d9dc3f33dac76f8d
Arthur Deierlein <110528300+C0rydoras@users.noreply.github.com>
2023-05-04 13:50:55 +0200

    feat!: containerized ember

--- ember/mirage/config.js
+++ ember/mirage/config.js
@@ -19,6 +19,7 @@ export default function () {
 
     https://www.ember-cli-mirage.com/docs/route-handlers/shorthands
   */
+  this.namespace = '/api';
   this.resource('projects');
   this.resource('dependency-versions');
   this.resource('dependencies');
