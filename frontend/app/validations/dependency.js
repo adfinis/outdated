e4a9c7f3ca8e405751778148a3488060d7a8dd57
Arthur <arthur.deierlein@adfinis.com>
2023-05-04 12:06:22 +0200

    feat: simplify project form to make it match new models

--- frontend/app/validations/dependency.js
+++
@@ -1,8 +0,0 @@
-import {
-  validatePresence,
-  validateLength,
-} from 'ember-changeset-validations/validators';
-
-export default {
-  name: [validatePresence(true), validateLength({ max: 100 })],
-};
