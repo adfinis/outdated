b4c57838a779b29f7edd6a610218fe5c5b66b817
Arthur <arthur.deierlein@adfinis.com>
2023-05-04 09:33:06 +0200

    chore: adjusted factories for reworked models

--- api/outdated/outdated/factories.py
+++ api/outdated/outdated/factories.py
@@ -6,35 +6,72 @@ from factory.django import DjangoModelFactory
 
 from . import models
 
+DEPENDENCIES = [
+    ("django", "PIP"),
+    ("djangorestframework", "PIP"),
+    ("djangorestframework-jsonapi", "PIP"),
+    ("django-filter", "PIP"),
+    ("django-cors-headers", "PIP"),
+    ("django-extensions", "PIP"),
+    ("django-debug-toolbar", "PIP"),
+    ("django-environ", "PIP"),
+    ("flake8", "PIP"),
+    ("flake8-bugbear", "PIP"),
+    ("flake8-tuple", "PIP"),
+    ("flake8-isort", "PIP"),
+    ("flake8-debugger", "PIP"),
+    ("pytest", "PIP"),
+    ("pytest-django", "PIP"),
+    ("pytest-cov", "PIP"),
+    ("pytest-vcr", "PIP"),
+    ("pytest-factoryboy", "PIP"),
+    ("python-dateutil", "PIP"),
+    ("ember-source", "NPM"),
+    ("ember-cli", "NPM"),
+    ("ember-data", "NPM"),
+    ("ember-cli-htmlbars", "NPM"),
+    ("ember-cli-htmlbars-inline-precompile", "NPM"),
+    ("ember-cli-babel", "NPM"),
+    ("ember-cli-qunit", "NPM"),
+    ("@glimmer/component", "NPM"),
+    ("@glimmer/tracking", "NPM"),
+]
+
 
 class DependencyFactory(DjangoModelFactory):
-    name = Faker("name")
+    name, provider = random.choice(DEPENDENCIES)
 
     class Meta:
         model = models.Dependency
 
 
-class DependencyVersionFactory(DjangoModelFactory):
+class ReleaseVersionFactory(DjangoModelFactory):
     class Meta:
-        model = models.DependencyVersion
+        model = models.ReleaseVersion
 
     dependency = SubFactory(DependencyFactory)
-    version = ".".join([str(random.randint(0, 9)) for _ in range(3)])
-    release_date = Faker(
-        "date_between_dates",
-        date_start=date.today() - timedelta(days=80),
-        date_end=date.today(),
-    )
-    end_of_life_date = Faker(
-        "date_between_dates",
-        date_start=release_date,
-        date_end=date.today() + timedelta(days=80),
-    )
+    end_of_life = Faker("date_this_year")
+    major_version = random.randint(0, 10)
+    minor_version = random.randint(0, 10)
 
     class Params:
-        outdated = Trait(end_of_life_date=date.today() - timedelta(days=80))
-        warning = Trait(end_of_life_date=date.today() + timedelta(days=20))
-        up_to_date = Trait(end_of_life_date=date.today() + timedelta(days=31))
+        undefined = Trait(end_of_life=None)
+        outdated = Trait(end_of_life=date.today())
+        warning = Trait(
+            end_of_life=date.today() + timedelta(days=150),
+        )
+        up_to_date = Trait(
+            end_of_life=date.today() + timedelta(days=365),
+        )
+
+
+class VersionFactory(DjangoModelFactory):
+    release_version = SubFactory(ReleaseVersionFactory)
+    patch_version = random.randint(0, 28)
+    release_date = Faker("date_this_year")
+
+    class Meta:
+        model = models.Version
 
 
 class ProjectFactory(DjangoModelFactory):
@@ -42,10 +79,12 @@ class ProjectFactory(DjangoModelFactory):
     repo = Sequence(lambda n: "https://github.com/userorcompany/%s/" % n)
 
     @post_generation
-    def dependency_versions(self, create, extracted, **kwargs):
-        if not create or not extracted:
-            return
-        self.dependency_versions.add(*extracted)
+    def versioned_dependencies(self, create, extracted, **kwargs):
+        if not create:
+            return  # pragma: no cover
+        if extracted:
+            for versioned_dependency in extracted:
+                self.versioned_dependencies.add(versioned_dependency)
 
     class Meta:
         model = models.Project
