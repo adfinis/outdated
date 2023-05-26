450759cd733748806cb10b81091967b33c2c81e6
Arthur <arthur.deierlein@adfinis.com>
2023-05-26 10:36:31 +0200

    chore: added tests for authentication

--- api/outdated/conftest.py
+++ api/outdated/conftest.py
@@ -1,23 +1,58 @@
+from functools import partial
+
 import pytest
-from django.contrib.auth.models import User
 from pytest_factoryboy import register
 from rest_framework.test import APIClient
 
+from outdated.oidc_auth.models import OIDCUser
+
 from .outdated import factories
+from .user.factories import UserFactory
 
 register(factories.DependencyFactory)
 register(factories.VersionFactory)
 register(factories.ReleaseVersionFactory)
 register(factories.ProjectFactory)
+register(UserFactory)
+
+
+def _get_claims(
+    settings,
+    id_claim="00000000-0000-0000-0000-000000000000",
+    groups_claim=None,
+    email_claim="test@example.com",
+    first_name_claim="foo",
+    last_name_claim="bar",
+    username_claim="foobar",
+):
+    groups_claim = groups_claim if groups_claim else []
+    return {
+        settings.OIDC_CLAIMS["ID"]: id_claim,
+        settings.OIDC_CLAIMS["GROUPS"]: groups_claim,
+        settings.OIDC_CLAIMS["EMAIL"]: email_claim,
+        settings.OIDC_CLAIMS["FIRST_NAME"]: first_name_claim,
+        settings.OIDC_CLAIMS["LAST_NAME"]: last_name_claim,
+        settings.OIDC_CLAIMS["USERNAME"]: username_claim,
+    }
 
 
 @pytest.fixture
-def client(db, settings):
+def get_claims(settings):
+    return partial(_get_claims, settings)
+
+
+@pytest.fixture
+def claims(settings):
+    return _get_claims(settings)
+
+
+@pytest.fixture
+def client(db, settings, get_claims):
     """Return rest framework client, includes db."""
     client = APIClient()
-    user = User(
+    user = OIDCUser(
         "sometoken",
-        {settings.OIDC_ID_CLAIM: "user", settings.OIDC_EMAIL_CLAIM: "test@example.com"},
+        get_claims(id_claim="user", groups_claim=[], email_claim="user@example.com"),
     )
     client.force_authenticate(user=user)
     client.user = user
--- api/outdated/oidc_auth/models.py
+++ api/outdated/oidc_auth/models.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.db.models import Q
 
 from outdated.user.models import User
 
