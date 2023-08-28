c36ae6acae8cff7130a3a824c791958039a44e31
Arthur Deierlein <arthur.deierlein@adfinis.com>
2023-08-28 14:27:42 +0200

    feat(api/commands): added project base command

--- api/outdated/outdated/management/commands/syncproject.py
+++ api/outdated/outdated/management/commands/syncproject.py
@@ -1,21 +1,11 @@
-from django.core.management.base import BaseCommand
-
-from outdated.outdated.models import Project
+from outdated.commands import ProjectCommand
 from outdated.outdated.synchroniser import Synchroniser
 
 
-class Command(BaseCommand):
+class Command(ProjectCommand):
     help = "Syncs the given project with its remote counterpart."
 
-    def add_arguments(self, parser):
-        parser.add_argument("project_name", type=str)
-
-    def handle(self, *_, **options):
-        project_name = options["project_name"]
-        try:
-            project = Project.objects.get(name__iexact=project_name)
-            self.stdout.write(f"Syncing project {project}")
-            Synchroniser(project).sync()
-            self.stdout.write(f"Finished syncing {project}")
-        except Project.DoesNotExist:
-            self.stdout.write(f"Project {project_name} not found")
+    def _handle(self, project):
+        self.stdout.write(f"Syncing project {project}")
+        Synchroniser(project).sync()
+        self.stdout.write(f"Finished syncing {project}")
