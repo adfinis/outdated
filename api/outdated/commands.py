bfff011758cd4eed60da5f7db335ddf70880d23c
Arthur Deierlein <arthur.deierlein@adfinis.com>
2023-08-28 14:27:42 +0200

    feat(api/commands)!: added project base command

--- api/outdated/commands.py
+++ api/outdated/commands.py
@@ -1,10 +1,41 @@
-from asyncio import run
+from django.core.management.base import BaseCommand, CommandParser
 
-from django.core.management.base import BaseCommand
+from outdated.outdated.models import Project
 
 
-class AsyncCommand(BaseCommand):
-    """Base command to run async code."""
+class ProjectCommand(BaseCommand):
+    def add_arguments(self, parser: CommandParser):
+        projects = parser.add_mutually_exclusive_group(required=True)
+        projects.add_argument(
+            "--all",
+            action="store_true",
+            help="Affect all projects",
+        )
+        projects.add_argument("projects", nargs="*", type=str, default=[])
 
     def handle(self, *args, **options):
-        run(self._handle(*args, **options))
+        projects = []
+        if not options["all"]:
+            nonexistant_projects = []
+            project_names = options["projects"]
+            for name in project_names:
+                try:
+                    projects.append(Project.objects.get(name__iexact=name))
+                except Project.DoesNotExist:
+                    nonexistant_projects.append(name)
+
+            if nonexistant_projects:
+                self.stderr.write(
+                    f"Projects with names {nonexistant_projects} do not exist"
+                )
+                return
+        projects = (
+            Project.objects.filter(id__in=[project.pk for project in projects])
+            or Project.objects.all()
+        )
+
+        for project in projects:
+            self._handle(project)
+
+    def _handle(self, project: Project): # pragma: no cover
+        raise NotImplementedError()
