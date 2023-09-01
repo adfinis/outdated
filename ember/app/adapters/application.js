7196640da99c5f12683ae0c588934fe82d5494ae
Arthur Deierlein <arthur.deierlein@adfinis.com>
2023-09-01 17:24:41 +0200

    fix(ember): update projects after creation / being edited

--- ember/app/adapters/application.js
+++ ember/app/adapters/application.js
@@ -5,7 +5,34 @@ export default class ApplicationAdapter extends OIDCJSONAPIAdapter {
   @service session;
 
   namespace = 'api';
+
   get headers() {
     return { ...this.session.headers, 'Content-Language': 'en-us' };
+  }
+
+  _appendInclude(url, adapterOptions) {
+    if (adapterOptions?.include) {
+      return `${url}?include=${adapterOptions.include}`;
+    }
+
+    return url;
+  }
+
+  urlForUpdateRecord(...args) {
+    const [, , { adapterOptions }] = args;
+
+    return this._appendInclude(
+      super.urlForUpdateRecord(...args),
+      adapterOptions,
+    );
+  }
+
+  urlForCreateRecord(...args) {
+    const [, { adapterOptions }] = args;
+
+    return this._appendInclude(
+      super.urlForCreateRecord(...args),
+      adapterOptions,
+    );
   }
 }
