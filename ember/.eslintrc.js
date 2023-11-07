2ce4218c4d018b1af88539acdf40178841e6a96e
Arthur Deierlein <arthur.deierlein@adfinis.com>
2023-11-07 15:23:53 +0100

    chore(ember/deps): add ember-template-imports and its prettier plugin

--- ember/.eslintrc.js
+++ ember/.eslintrc.js
@@ -1,8 +1,26 @@
-'use strict';
-
 module.exports = {
   settings: {
     'import/internal-regex': '^outdated/',
   },
   extends: ['@adfinis/eslint-config/ember-app'],
+  overrides: [
+    {
+      files: ['**/*.{js,ts}'],
+      rules: {
+        'ember/no-replace-test-comments': 'error',
+      },
+    },
+    {
+      files: ['**/*.gjs'],
+      parser: 'ember-eslint-parser',
+      plugins: ['ember'],
+      extends: ['plugin:ember/recommended-gjs'],
+    },
+    {
+      files: ['tests/**/*.{js,ts,gjs,gts}'],
+      rules: {
+        'ember/no-replace-test-comments': 'error',
+      },
+    },
+  ],
 };
