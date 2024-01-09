0dc5574a4392645ed0bf92a060b553d7ad969998
Arthur Deierlein <arthur.deierlein@adfinis.com>
2024-01-09 15:20:54 +0100

    feat(ember): adjusted project-form to allow using access tokens

--- ember/app/validations/project.js
+++ ember/app/validations/project.js
@@ -2,16 +2,31 @@ import {
   validatePresence,
   validateLength,
   validateFormat,
+  validateInclusion,
 } from 'ember-changeset-validations/validators';
 
+import validationsWhenOther from 'outdated/utils/validations-when-other';
+
 export default {
-  name: [validatePresence(true), validateLength({ max: 100 })],
+  name: [
+    validatePresence({ presence: true, ignoreBlank: true }),
+    validateLength({ max: 100 }),
+  ],
+  repoType: [validatePresence(true)],
   repo: [
-    validatePresence(true),
+    validatePresence({ presence: true, ignoreBlank: true }),
     validateLength({ max: 200 }),
     validateFormat({
       regex: /^([-_\w]+\.[-._\w]+)\/([-_\w]+)\/([-_\w]+)$/,
     }),
   ],
+  accessToken: validationsWhenOther({
+    field: 'repoType',
+    otherFieldValidator: validateInclusion({ in: ['access-token'] }),
+    fieldValidators: [
+      validatePresence({ presence: true, ignoreBlank: true }),
+      validateFormat({ regex: /^[-_a-zA-Z\d]*$/ }),
+    ],
+  }),
   maintainers: [],
 };
