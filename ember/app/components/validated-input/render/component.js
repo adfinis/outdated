dfbd6e722ff660ce1fbd2dded1db3dc394ba5f15
Arthur Deierlein <arthur.deierlein@adfinis.com>
2024-01-09 13:31:01 +0100

    feat(ember): add hidden property to inputs

--- ember/app/components/validated-input/render/component.js
+++ ember/app/components/validated-input/render/component.js
@@ -1,9 +1,17 @@
+import { scheduleOnce } from '@ember/runloop';
 import Component from '@glimmer/component';
 import getMessages from 'ember-changeset-validations/utils/get-messages';
 import PowerSelect from 'ember-power-select/components/power-select';
 import PowerSelectMultiple from 'ember-power-select/components/power-select-multiple';
 
 export default class RenderComponent extends Component {
+  constructor(...args) {
+    super(...args);
+
+    if (typeof this.args.hidden === 'boolean') {
+      scheduleOnce('actions', this.args, 'validateModel', this.args.model);
+    }
+  }
   get selectComponent() {
     return this.args.multiple ? PowerSelectMultiple : PowerSelect;
   }
